7d58139307021a373412938bf25ed8373fee9bdf
Ranjay Kumar <ranjaykumar73.rk@gmail.com>
2025-07-10 12:59:47 +0530

    changes in login modal

--- src/pages/Login/LoginModal.js
+++ src/pages/Login/LoginModal.js
@@ -3,10 +3,13 @@ import { Modal } from 'react-bootstrap';
 import Login1 from './Login1';
 
 const LoginModal = ({ show, onClose, onLoginSuccess }) => {
-
+ const modalStyle = {
+  height: "80vh",
+  overflowY: "hidden",
+};
   return (
-    <Modal show={show} onHide={onClose} size="lg" centered backdrop="static">
-      <Modal.Body className="p-0" >
+    <Modal show={show} onHide={onClose} size="lg" centered backdrop="static"  dialogClassName="custom-modal-height">
+      <Modal.Body className="p-0"style={modalStyle} >
         <Login1 onClose={onClose} onLoginSuccess={onLoginSuccess} />
       </Modal.Body>
     </Modal>
